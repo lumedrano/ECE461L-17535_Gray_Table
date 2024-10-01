8321d429eba032ab38d63a262ca0d14e359ebb69
Luigi Medrano <luigimedrano03@gmail.com>
2024-09-30 21:20:19 -0500

    database.py file upload

---
+++ src/server/database.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_database():
+   connection_string = os.getenv('CONNECTION_STRING')
+   try:
+      client = MongoClient(connection_string, server_api=ServerApi('1'))
+   except Exception:
+      print("Error connecting to MongoClient")
+      return None
+   return client
+
+if __name__ == "__main__":
+    get_database()
