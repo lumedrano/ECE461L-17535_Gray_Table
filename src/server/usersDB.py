2e82a88e227bc5457eadb820b0a7d3d497954510
Luigi Medrano <luigimedrano03@gmail.com>
2024-10-02 11:42:47 -0500

    centralizing database

--- src/server/usersDB.py
+++ src/server/usersDB.py
@@ -1,13 +1,8 @@
-#Kimberly & Luigi
-
-#using MongoDB through python
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
+from database import get_database
 import cipher
 
 
-#for reference
+    #for reference
 '''
 Structure of User entry:
 User = {
@@ -18,89 +13,86 @@ User = {
 }
 '''
 
-# load_dotenv()
 
-# connection_string = os.getenv('MONGODB_CONNECTION_STRING')
+# Global variables for client and collection
+client = None
+collection = None
 
-# client = MongoClient(connection_string)
+def get_collection():
+    global client, collection
+    if collection is None:
+        client = get_database()
+        if client is not None:
+            db = client['ece461l_final_project']
+            collection = db['usersDB']
+        else:
+            raise Exception("Failed to connect to the database")
+    return collection
 
-# # Access a database
-# db = client['ece461l_final_project']
+def addUser(username, userId, password):
+    try:
+        collection = get_collection()
+        if collection.find_one({'username': username, 'userId': userId}):
+            return "User already exists."
+        encryptedpassword = cipher.encrypt(password, 3, 1)
+        new_user = {
+            'username': username,
+            'userId': userId,
+            'password': encryptedpassword,
+            'projects': []
+        }
+        result = collection.insert_one(new_user)
+        if result.acknowledged:
+            return "User was added successfully."
+        else:
+            return "Failed to add user."
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
-# # Access a collection
-# collection = db['usersDB']
+def __queryUser(username, userId):
+    try:
+        collection = get_collection()
+        return collection.find_one({'username': username, 'userId': userId})
+    except Exception as e:
+        print(f"An error occurred while querying user: {str(e)}")
+        return None
 
-
-# Function to add a new user
-def addUser(client, username, userId, password):
-    # Add a new user to the database
-    if collection.find_one({'username': username, 'userId': userId}):
-        return "User already exist."
-    encryptedpassword = cipher.encrypt(password, 3, 1)
-    new_user = {
-        'username': username,
-        'userId': userId,
-        'passsword': encryptedpassword,
-        'projects': []
-    }
-    
-    result = collection.insert_one(new_user)
-    if result.acknowledged:
-        return "User wass added successfully."
-    else:
-        return "Failed to add user."
-
-# Helper function to query a user by username and userId
-def __queryUser(client, username, userId):
-    # Query and return a user from the database
-    user = collection.find_one({'username': username, 'userId': userId})
-    return user
-
-# Function to log in a user
-#TODO: test and check with Kimberly
 def login(username, userId, password):
+    try:
+        user = __queryUser(username, userId)
+        if user:
+            encrypted_pw = user['password']
+            decrypted_pw = cipher.decrypt(encrypted_pw, 3, 1)
+            if decrypted_pw == password:
+                return "Login Successful!"
+            return "Invalid password."
+        return "User not found."
+    except Exception as e:
+        return f"An error occurred during login: {str(e)}"
 
-    #find person using query function from above
-    user = __queryUser(username, userId)
-    #check if user is found, if so then use decrypt function on password by passing password in
-    if user:
-        encrypted_pw = user['password']
-        decrypted_pw = cipher.decrypt(encrypted_pw, 3, 1)
-        
-        #if match then return something that they are logged in
-        if decrypted_pw == password:
-            return "Login Successful!"
-        #if not, return error message saying invalid 
-        return "Invalid password."
-    #if user was not found by username/id then return user not found
-    return "User not found."
-
-# Function to add a user to a project
 def joinProject(userId, projectId):
-    # Add a user to a specified project
+    try:
+        collection = get_collection()
+        if not collection.find_one({'userId': userId}):
+            return "User not found"
+        result = collection.update_one(
+            {'userId': userId},
+            {'$addToSet': {'projects': projectId}}
+        )
+        if result.modified_count > 0:
+            return "Successfully added to project!"
+        else:
+            return "Already added to project."
+    except Exception as e:
+        return f"An error occurred while joining project: {str(e)}"
 
-    if not collection.find_one({'userId': userId}):
-        return "User not found"
-    
-    result = collection.update_one(
-        {'userId': userId},
-        {'$addToSet':{'projectId': projectId}}
-    )
-
-    if result.modified_count > 0:
-        return "Successfully added to project!"
-    else:
-        "Already added to project."
-
-# Function to get the list of projects for a user
 def getUserProjectsList(userId):
-    # Get and return the list of projects a user is part of
-
-    #find user in db
-    user = collection.find_one({'userId': userId})
-    
-    # if found use .get to obtain projects
-    if user:
-        return user.get('projects', [])
-    else:
-        return "user not found"
+    try:
+        collection = get_collection()
+        user = collection.find_one({'userId': userId})
+        if user:
+            return user.get('projects', [])
+        else:
+            return "User not found"
+    except Exception as e:
+        return f"An error occurred while getting user projects: {str(e)}"
