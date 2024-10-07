5b7fec599047923af73eccc92703d074480965d2
Sean Paslak <spaslak228@utexas.edu>
2024-10-07 17:08:07 -0500

    Initial implementations of the functions

--- src/server/hardwareDB.py
+++ src/server/hardwareDB.py
@@ -1,4 +1,6 @@
 # Import necessary libraries and modules
+from dataclasses import dataclass
+
 from pymongo import MongoClient
 
 '''
@@ -9,28 +11,43 @@ HardwareSet = {
     'availability': initCapacity
 }
 '''
-
+client = MongoClient('localhost', 27017)
+database = client['hwSetName']
+collection = database['hwSetName']
 # Function to create a new hardware set
 def createHardwareSet(client, hwSetName, initCapacity):
     # Create a new hardware set in the database
-    pass
+    HardwareSet = {
+        'hwName': hwSetName,
+        'capacity': initCapacity,
+        'availability': initCapacity
+    }
+    collection.insert_one(HardwareSet)
 
 # Function to query a hardware set by its name
 def queryHardwareSet(client, hwSetName):
     # Query and return a hardware set from the database
-    pass
+    query = collection.find_one({'hwName': hwSetName})
 
 # Function to update the availability of a hardware set
 def updateAvailability(client, hwSetName, newAvailability):
     # Update the availability of an existing hardware set
-    pass
+    query = collection.find_one({'hwName': hwSetName})
+    query['availability'] = newAvailability
 
 # Function to request space from a hardware set
 def requestSpace(client, hwSetName, amount):
     # Request a certain amount of hardware and update availability
-    pass
+    set = queryHardwareSet(client, hwSetName)
+    if set['availability'] >= amount:
+        collection.update_one(
+            {'hwName': hwSetName},
+            {'$inc': {'availability': - amount}}
+        )
 
 # Function to get all hardware set names
 def getAllHwNames(client):
     # Get and return a list of all hardware set names
-    pass
+    setNames = collection.find({}, {'hwName': 1, "_id": 0})
+    list = [hw['hwName'] for hw in setNames]
+    return list
