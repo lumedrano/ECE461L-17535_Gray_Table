00094d499801ff04ec17fa5ffab98c2d8c213010
Luigi Medrano <luigimedrano03@gmail.com>
2024-10-07 10:21:13 -0500

    react app created

---
+++ src/client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Custom React App!</h1>
+        <p>Building the frontend with React is fun!</p>
+        <a
+          className="App-link"
+          href="https://www.example.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit my portfolio here!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
