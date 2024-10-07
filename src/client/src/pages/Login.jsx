d611b98487f571bbccee4bc700a7136b3c551c41
Luigi Medrano <luigimedrano03@gmail.com>
2024-10-07 16:57:26 -0500

    react dev start

---
+++ src/client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logincomponents from './components/logincomponents/logincomponent'
+
+function Login(){
+    return (
+        <div className='page-container'>
+            <logincomponents />
+        </div>
+    );
+}
+
+export default Login;
