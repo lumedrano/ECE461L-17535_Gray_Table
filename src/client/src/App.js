dd56d4620ac2d618f2bab90611d72d503a8657e5
Luigi Medrano <luigimedrano03@gmail.com>
2024-10-08 09:51:12 -0500

    create routes for login page and projects page

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/" element={<Login/>}></Route>
           <Route path="/login" element={<Login/>}></Route>
+          <Route path="/projects" element={<Home/>}></Route>
         </Routes>
       </Router>
     </div>
